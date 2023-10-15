aa68e59f0c2961b14e1f88dfaa2a64a76d0d0cd6
Aaron <iaaron.xyz@outlook.com>
2023-10-14 21:29:16 -0600

    setup workplace

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      Hello Mom! This is my Porfolio to get a good job and buy you a beautiful
+      house!
+    </>
+  );
+}
+
+export default App;
