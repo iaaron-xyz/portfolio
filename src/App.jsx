9d65cf255b1cc68175e2633dd3429f385a03a59b
Aaron <iaaron.xyz@outlook.com>
2023-10-15 02:57:51 -0600

    setup tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import "./App.css";
 
 function App() {
   return (
-    <>
+    <h1 className='text-4xl'>
       Hello Mom! This is my Porfolio to get a good job and buy you a beautiful
       house!
-    </>
+    </h1>
   );
 }
 
