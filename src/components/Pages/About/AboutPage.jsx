5b4e69071e47a0495cce1cc644cb076b7479e93c
Aaron <iaaron.xyz@outlook.com>
2023-12-28 02:41:41 -0600

    add skills section

--- src/components/Pages/About/AboutPage.jsx
+++ src/components/Pages/About/AboutPage.jsx
@@ -2,7 +2,8 @@ const AboutPage = () => {
   return (
     <div className='w-full flex flex-col align-center justify-center'>
       <AboutCover />
-      <AboutContent />
+      <AboutContentDesc />
+      <AboutContentSkills />
     </div>
   );
 };
@@ -15,7 +16,7 @@ const AboutCover = () => {
   );
 };
 
-const AboutContent = () => {
+const AboutContentDesc = () => {
   return (
     <div className='flex'>
       {/* title */}
@@ -27,6 +28,32 @@ const AboutContent = () => {
           developer. As a developer I am specialized as Front-end React
           Developer who loves creating beautiful sites and web apps that are interactive, responsive and easy to use.`}
         />
+        <AboutMeSectionTextParagraph
+          p={`I can build front-end web apps using only vanilla JavaScript, HTML and CSS but I am also capable to complete projects using the library React or directly an entire framework like Next.js.`}
+        />
+        <AboutMeSectionTextParagraph
+          p={`Checkout my completed projects and give a look at my completed projects in the projects section.`}
+        />
+        <AboutMeSectionTextParagraph
+          p={`Or in case you are interested, I also can work as a 3D artist and as a computational and data scientist.`}
+        />
+      </div>
+    </div>
+  );
+};
+
+const AboutContentSkills = () => {
+  return (
+    <div className='flex mt-48'>
+      {/* title */}
+      <AboutMeSectionTitle title='My Skills & Knowledge' />
+      {/* Text content */}
+      <div className='flex-1 text-2xl'>
+        <AboutMeSectionTextParagraph
+          p={`Hi! My name is Aaron, I am a multidisciplinary scientist, artist and
+        developer. As a developer I am specialized as Front-end React
+        Developer who loves creating beautiful sites and web apps that are interactive, responsive and easy to use.`}
+        />
       </div>
     </div>
   );
@@ -35,7 +62,7 @@ const AboutContent = () => {
 const AboutMeSectionTitle = ({ title }) => {
   return (
     <div className='flex-1'>
-      <h2 className='text-6xl font-chillaxBold pl-48 pr-12 text-right'>
+      <h2 className='text-6xl font-chillaxBold pl-48 pr-12 text-center'>
         {title}
       </h2>
     </div>
